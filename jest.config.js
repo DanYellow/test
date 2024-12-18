7df6ca09a82534767f63e01738d5bebbf46434f9
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-18 13:46:49 +0100

    Fix config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
     verbose: true,
     testMatch: ["**/tests/**/*.(spec|test).[jt]s?(x)"],
-    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+    // transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
     // transform: {
     //     '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
     // },

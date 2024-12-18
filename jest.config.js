7e66b70a39f388199a576e900df7a15a8fc4ed78
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-18 13:12:21 +0100

    revert

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
     verbose: true,
     testMatch: ["**/tests/**/*.(spec|test).[jt]s?(x)"],
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
-    transform: {
-        '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
-    },
+    // transform: {
+    //     '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    // },
 };

d42885c6e8140540c046e125afd929c13ebfafae
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-23 18:08:22 +0100

    Add python script for request

--- get-lastprod.py
+++ get-lastprod.py
@@ -17,12 +17,12 @@ headers = {
     'Authorization': f'Bearer {args["gh-token"]}',
     'X-GitHub-Api-Version': '2022-11-28',
 }
-r = requests.get(
+req = requests.get(
     'https://api.github.com/repos/DanYellow/test/actions/workflows/133863234/runs?status=completed&per_page=2',
     headers=headers
 )
 
-response = r.json()
-
-with open(env_file, "a") as myfile:
-    myfile.write(f"LAST_COMPLETED_RUN_ID={response.workflow_runs[0].id}")
+response = req.json()
+print(response)
+# with open(env_file, "a") as myfile:
+#     myfile.write(f"LAST_COMPLETED_RUN_ID={response.workflow_runs[0].id}")

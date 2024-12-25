40839adaee717bacbe0f4ce70dc65f6424a66d21
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2024-12-25 19:28:43 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig, devices } from '@playwright/test';
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv
+ * https://github.com/motdotla/dotenv1
  */
 // require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 

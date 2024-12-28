687b10250a102db007640b1beeddf0548fe78087
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-28 18:28:48 +0100

    tessf

--- database.sh
+++ database.sh
@@ -3,6 +3,15 @@
 # echo "$1"
 # echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE'
 
+# mysql -u $MYSQL_USER \
+#     -p$MYSQL_PASSWORD \
+#     -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
+
+MYSQL_USER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER');
+MYSQL_PASSWORD = $(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD');
+MYSQL_SERVER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER');
+MYSQL_DATABASE = $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE');
+
 mysql -u $MYSQL_USER \
     -p$MYSQL_PASSWORD \
     -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql

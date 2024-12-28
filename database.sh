a7aaadbebdb015e0989c808057b4ae0003f192e3
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-28 18:30:43 +0100

    fefe

--- database.sh
+++ database.sh
@@ -7,17 +7,17 @@
 #     -p$MYSQL_PASSWORD \
 #     -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
 
-MYSQL_USER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER');
-MYSQL_PASSWORD = $(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD');
-MYSQL_SERVER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER');
-MYSQL_DATABASE = $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE');
+# MYSQL_USER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER');
+# MYSQL_PASSWORD = $(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD');
+# MYSQL_SERVER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER');
+# MYSQL_DATABASE = $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE');
 
-mysql -u $MYSQL_USER \
-    -p$MYSQL_PASSWORD \
-    -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
+# mysql -u $MYSQL_USER \
+#     -p$MYSQL_PASSWORD \
+#     -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
 
 
-# mysql -u $($SECRETS_CONTEXT | jq '.MYSQL_USER') \
-#     -p$($SECRETS_CONTEXT | jq '.MYSQL_PASSWORD') \
-#     -h $($SECRETS_CONTEXT | jq '.MYSQL_SERVER') $($SECRETS_CONTEXT | jq '.MYSQL_DATABASE') < database.sql
+mysql -u $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER') \
+    -p$(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD') \
+    -h $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER') $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE') < database.sql
 # mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql

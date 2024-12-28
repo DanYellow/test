5650fedfb444cf4e7743fe7108c4feb65809d11b
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-28 18:06:52 +0100

    jq

--- database.sh
+++ database.sh
@@ -1,4 +1,7 @@
 # echo $SECRETS_CONTEXT
 # echo "$SECRETS_CONTEXT"
-echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE'
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
+# echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE'
+mysql -u $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER') \
+    -p$(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD') \
+    -h $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER') $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE') < database.sql
+# mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql

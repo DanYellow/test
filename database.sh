bd86733f075dd0e5c6d6ff97d0e0a4c149c7e0aa
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-28 18:20:43 +0100

    echo

--- database.sh
+++ database.sh
@@ -2,7 +2,7 @@
 echo "$super_secret"
 echo "$1"
 # echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE'
-mysql -u $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER') \
-    -p$(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD') \
-    -h $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER') $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE') < database.sql
+mysql -u $($SECRETS_CONTEXT | jq '.MYSQL_USER') \
+    -p$($SECRETS_CONTEXT | jq '.MYSQL_PASSWORD') \
+    -h $($SECRETS_CONTEXT | jq '.MYSQL_SERVER') $($SECRETS_CONTEXT | jq '.MYSQL_DATABASE') < database.sql
 # mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql

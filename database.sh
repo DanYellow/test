7984a40521f3c97158694b1222356e8697b42bfd
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-28 19:34:56 +0100

    db

--- database.sh
+++ database.sh
@@ -12,11 +12,13 @@ MYSQL_PASSWORD = $(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD');
 MYSQL_SERVER = $(echo $SECRETS_CONTEXT | jq '.MYSQL_SERVER');
 MYSQL_DATABASE = $(echo $SECRETS_CONTEXT | jq '.MYSQL_DATABASE');
 
-mysql \
+mysql -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
+
+# mysql \
 # -u $MYSQL_USER \
     # -p$MYSQL_PASSWORD \
     # --ssl-mode=required \
-    -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
+    # -h $MYSQL_SERVER $MYSQL_DATABASE < database.sql
 
 # mysql -u $(echo $SECRETS_CONTEXT | jq '.MYSQL_USER' --raw-output) \
 #     -p $(echo $SECRETS_CONTEXT | jq '.MYSQL_PASSWORD' --raw-output) \

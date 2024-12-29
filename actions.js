62a6dc9dcc1ddc866357b68f32624e1d478e1693
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-29 15:54:30 +0100

    tes

--- actions.js
+++ actions.js
@@ -1,6 +1,9 @@
 import * as core from '@actions/core';
 
-core.info('\u001b[43mThis background will be yellow');
+// core.info('\u001b[43mThis background will be yellow');
 
 core.summary.addRaw('Some content here :speech_balloon:', true)
 core.summary.addLink('click here', 'https://github.com')
+
+
+core.summary.write()

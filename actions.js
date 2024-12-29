355cb735ef4e4d0955b2f650a7fec86e9ab91f4a
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-29 15:52:43 +0100

    tesf

--- actions.js
+++ actions.js
@@ -3,3 +3,4 @@ import * as core from '@actions/core';
 core.info('\u001b[43mThis background will be yellow');
 
 core.summary.addRaw('Some content here :speech_balloon:', true)
+core.summary.addLink('click here', 'https://github.com')

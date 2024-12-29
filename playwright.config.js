dc67231c2dab9f88451370849b282ec20d5cba1a
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-29 10:01:05 +0100

    baseURL

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    // url: baseURL,
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
 });

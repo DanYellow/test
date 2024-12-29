dd88970e4962b170eb123c161ddfc0a82ef12748
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-29 19:50:48 +0100

    testg

--- actions.js
+++ actions.js
@@ -1,7 +1,6 @@
 import * as core from "@actions/core";
 import path from "path";
 
-
 // core.info('\u001b[43mThis background will be yellow');
 
 // core.summary.addRaw('Some content here :speech_balloon:', true)
@@ -9,50 +8,105 @@ import path from "path";
 
 // core.summary.write()
 
+const processResults = (results) => {
+    const res = [];
+    results.forEach((element) => {
+        const payload = {};
+        payload.file = element.title;
+
+        res.push(payload);
+    });
+};
+
 class MyReporter {
+    constructor() {
+        this.suite = {};
+        this.results = {};
+    }
+
     onBegin(config, suite) {
-        console.dir(suite._entries[0]._entries)
-        if(process.env.CI) {
-            core.summary.addRaw(`Total tests: ${suite.allTests().length}`, true)
-            core.summary.write()
+        this.suite = suite;
+        // console.log(suite)
+        // this.results[suite.title] = suite.title;
+        // console.dir(suite._entries[0]._entries)
+        if (process.env.CI) {
+            core.summary.addRaw(
+                `Total tests: ${suite.allTests().length}`,
+                true
+            );
+            core.summary.write();
         }
-            // console.log(`Starting the run with ${suite.allTests().length} tests`);
+        // console.log(`Starting the run with ${suite.allTests().length} tests`);
     }
 
     onTestBegin(test, result) {
         // console.log(test._requireFile)
-        // console.log(result)
+        // console.log(test)
         // console.log(`Starting test ${test.title}`);
     }
 
     onTestEnd(test, result) {
-        if(process.env.CI) {
-            core.summary.addHeading(path.basename(test._requireFile), '2')
-            core.summary.addRaw(test.title, true)
-
-
-            const tableData = [
-                {data: 'Header1', header: true},
-                {data: 'Header2', header: true},
-                {data: 'Header3', header: true},
-                {data: 'MyData1'},
-                {data: 'MyData2'},
-                {data: 'MyData3'}
-              ]
-
-              // Add an HTML table
-              core.summary.addTable([tableData])
-
-              core.summary.write()
-        }
+        // if(process.env.CI) {
+        //     core.summary.addHeading(path.basename(test._requireFile), '2')
+        //     core.summary.addRaw(test.title, true)
+        //     const tableData = [
+        //         {data: 'Header1', header: true},
+        //         {data: 'Header2', header: true},
+        //         {data: 'Header3', header: true},
+        //         {data: 'MyData1'},
+        //         {data: 'MyData2'},
+        //         {data: 'MyData3'}
+        //       ]
+        //       // Add an HTML table
+        //       core.summary.addTable([tableData])
+        //       core.summary.write()
+        // }
         // console.dir(result.duration);
         // console.dir(test);
         // console.log(`Finished test ${test.title}: ${result.status} | ${result.duration / 1000}s`);
+        // console.dir(test);
     }
 
     onEnd(result) {
-        // console.dir(result);
-        // console.dir(tes);
+        this.suite?.suites.forEach((suite) => {
+            // console.log(suite.project())
+            // parent.title
+            const listTestFiles = suite
+                .allTests()
+                .map((test) => test.location.file)
+                .reduce((acc, curr) => {
+                    if (!acc.includes(curr)) {
+                        acc.push(curr);
+                    }
+
+                    return acc;
+                }, []);
+
+            const testsDict = listTestFiles.reduce((acc, curr) => {
+                acc[curr] = suite.allTests().filter((test) => {
+                    return test.location.file === curr;
+                });
+
+                return acc;
+            }, {});
+
+            for (const filePath of Object.keys(testsDict)) {
+                console.log(testsDict[filePath]);
+
+                if (process.env.CI) {
+                    core.summary.addHeading(
+                        path.basename(filePath),
+                        "2"
+                    );
+
+                    for (const test of testsDict[filePath]) {
+                        core.summary.addRaw(test.title, true);
+                    }
+                }
+            }
+        });
+        // console.dir(this.suite.suites[0]._entries[0]);
+        // console.dir(this.suite._entries);
         // console.log(`Finished the run: ${result.status}`);
     }
 }

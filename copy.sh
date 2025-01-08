8eb81479883da72d88a7eee33a4539b8221e3b2c
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-01-08 23:09:10 +0100

    fix: comment lines

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
-cp "$1/test.sh" "$1/dist"
-cp $1/rsync-exclude.txt $1/dist
-cp $1/consignes.js $1/dist
-cp $1/consignes.css $1/dist
+# cp "$1/test.sh" "$1/dist"
+# cp $1/rsync-exclude.txt $1/dist
+# cp $1/consignes.js $1/dist
+# cp $1/consignes.css $1/dist
 
 
 export VITE_BUILD_DATE=$(date + '%d/%m/%Y %H:%M:%S')

3602d22517f0f6654b953541bf742ac82baa4bf0
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-01-08 23:33:55 +0100

    fix: comment lines

--- src/main.js
+++ src/main.js
@@ -30,6 +30,10 @@ const initialPageTitle = document.title;
 const initialPageFavicon = faviconContainer.getAttribute("href");
 const listPokemon = [];
 
+console.log(import.meta.env.VITE_BUILD_DATE)
+console.log(import.meta.env)
+
+
 const setTitleTagForGeneration = () => {
     const allStickedHeaders = Array.from(document.querySelectorAll(".is-pinned"));
     let allStickedVisibleHeaders = allStickedHeaders.filter((item) => isElementInViewport(item));

725d453e83c8415e66ed6bec1921facf798fbac9
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-01-09 00:20:00 +0100

    fix: comment lines

--- copy.sh
+++ copy.sh
@@ -5,5 +5,8 @@
 
 echo "VITE_BUILD_DATE=$(date +'%d/%m/%Y %H:%M:%S')" >> $GITHUB_ENV
 echo "FOO=HELLO" >> $GITHUB_ENV
-echo "$env.VITE_AUTHOR"
+# echo collaborator="$(env.VITE_TEST) <<< $GITHUB_ENV"
+# echo "$collaborator"
 echo "VITE_BUILD_AUTHOR=$(echo ${{ github.actor }})" >> $GITHUB_ENV
+
+VITE_VARNAME="my value"
